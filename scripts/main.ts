d2d7908789694c27982f1b0309613a506a78cd17
Alien Edds <edddsallen@gmail.com>
2024-08-19 09:05:47 -0500

    main push

---
+++ scripts/main.ts
@@ -0,0 +1,16 @@
+import { EntityHealthComponent, world } from "@minecraft/server";
+
+
+world.beforeEvents.worldInitialize.subscribe((data) => {
+    const {itemComponentRegistry} = data
+    itemComponentRegistry.registerCustomComponent("basic_addon:item_function", {
+        onUse: (data) => {
+            const {source} = data
+            const entity = source.getEntitiesFromViewDirection()[0]
+            if (!entity) return
+            const comp = entity.entity.getComponent(EntityHealthComponent.componentId) as EntityHealthComponent
+            if (!comp) return
+            source.onScreenDisplay.setActionBar(`${comp.currentValue}`)
+        }
+    })
+})
