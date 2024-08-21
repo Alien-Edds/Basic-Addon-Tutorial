1307abf643ea296c67b96da47caef035be5bcb7e
Alien Edds <edddsallen@gmail.com>
2024-08-21 11:44:44 -0500

    push

--- BP/scripts/main.js
+++ BP/scripts/main.js
@@ -1,6 +1,6 @@
 import { EntityHealthComponent, world } from "@minecraft/server";
 world.beforeEvents.worldInitialize.subscribe((data) => {
-    const { itemComponentRegistry } = data;
+    const { itemComponentRegistry, blockComponentRegistry } = data;
     itemComponentRegistry.registerCustomComponent("basic_addon:item_function", {
         onUse: (data) => {
             const { source } = data;
@@ -11,6 +11,22 @@ world.beforeEvents.worldInitialize.subscribe((data) => {
             if (!comp)
                 return;
             source.onScreenDisplay.setActionBar(`${comp.currentValue}`);
+        }
+    });
+    blockComponentRegistry.registerCustomComponent("basic_addon:block_function", {
+        onPlayerInteract: (data) => {
+            const block = data.block;
+            const state = block.permutation.getState("basic_addon:state_1");
+            if (state === undefined)
+                return;
+            switch (state) {
+                case 0:
+                    block.setPermutation(block.permutation.withState("basic_addon:state_1", 1));
+                    break;
+                case 1:
+                    block.setPermutation(block.permutation.withState("basic_addon:state_1", 0));
+                    break;
+            }
         }
     });
 });
--- scripts/main.ts
+++ scripts/main.ts
@@ -1,8 +1,8 @@
-import { EntityHealthComponent, world } from "@minecraft/server";
+import { EntityEquippableComponent, EntityHealthComponent, EquipmentSlot, world } from "@minecraft/server";
 
 
 world.beforeEvents.worldInitialize.subscribe((data) => {
-    const {itemComponentRegistry} = data
+    const {itemComponentRegistry, blockComponentRegistry} = data
     itemComponentRegistry.registerCustomComponent("basic_addon:item_function", {
         onUse: (data) => {
             const {source} = data
@@ -11,6 +11,21 @@ world.beforeEvents.worldInitialize.subscribe((data) => {
             const comp = entity.entity.getComponent(EntityHealthComponent.componentId) as EntityHealthComponent
             if (!comp) return
             source.onScreenDisplay.setActionBar(`${comp.currentValue}`)
+        }
+    })
+    blockComponentRegistry.registerCustomComponent("basic_addon:block_function", {
+        onPlayerInteract: (data) => {
+            const block = data.block
+            const state = block.permutation.getState("basic_addon:state_1")
+            if (state === undefined) return
+            switch (state) {
+                case 0 :
+                    block.setPermutation(block.permutation.withState("basic_addon:state_1", 1))
+                    break
+                case 1 :
+                    block.setPermutation(block.permutation.withState("basic_addon:state_1", 0))
+                    break
+            }
         }
     })
 })
